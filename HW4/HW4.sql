f037fdd2e2f5e4eb24a3d2e9bc229844c1d8516e
XinqiW <69182292+XinqiW@users.noreply.github.com>
2020-10-07 21:27:39 +0200

    Add files via upload

--- HW4/HW4.sql
+++ HW4/HW4.sql
@@ -1,1 +1,17 @@
-
+-- INNER join orders,orderdetails,products and customers. Return back:
+-- orderNumber
+-- priceEach
+-- quantityOrdered
+-- productName
+-- productLine
+-- city
+-- country
+-- orderDate
+SELECT orderNumber, priceEach, quantityOrdered, productName, productLine, city, country, orderDate
+FROM orders t1
+INNER JOIN orderdetails t2
+USING (orderNumber)
+INNER JOIN products t3
+USING (productCode)
+INNER JOIN customers t4
+USING (customerNumber);
